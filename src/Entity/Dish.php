a9562ad08daab4da01bfba92e9e3caa92bf32f1f
rreynaleon <ro.reyna83@gmail.com>
2026-03-10 11:27:00 +0100

    Modification Entity Dish

--- src/Entity/Dish.php
+++ src/Entity/Dish.php
@@ -7,37 +7,41 @@ use App\Repository\DishRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: DishRepository::class)]
 #[ORM\Table(name: 'dishes')]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['dish:read']],
+    denormalizationContext: ['groups' => ['dish:write']]
+)]
 class Dish
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['dish:read', 'menu:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 100, unique: true)]
+    #[Groups(['dish:read', 'menu:read', 'dish:write'])]
     private ?string $title = null;
 
     #[ORM\Column(type: 'text', nullable: true)]
+    #[Groups(['dish:read', 'dish:write'])]
     private ?string $photos = null;
 
-    /**
-     * Owning side of the ManyToMany — maps to the `dish_allergens` join table.
-     *
-     * @var Collection<int, Allergen>
-     */
     #[ORM\ManyToMany(targetEntity: Allergen::class, inversedBy: 'dishes')]
     #[ORM\JoinTable(
         name: 'dish_allergens',
         joinColumns: [new ORM\JoinColumn(name: 'dish_id', referencedColumnName: 'id', onDelete: 'CASCADE')],
         inverseJoinColumns: [new ORM\JoinColumn(name: 'allergen_id', referencedColumnName: 'id', onDelete: 'CASCADE')],
     )]
+    #[Groups(['dish:read'])]
     private Collection $allergens;
 
     #[ORM\OneToMany(targetEntity: MenuDish::class, mappedBy: 'dish')]
+    // Pas de Groups ici pour éviter la référence circulaire
     private Collection $menuDishes;
 
     public function __construct()
